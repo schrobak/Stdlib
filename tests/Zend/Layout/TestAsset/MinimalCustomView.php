8148ca6a0e1dc8836d523ce09ad26fe3f2edffd1
Ralph Schindler <ralph.schindler@zend.com>
2010-06-16 10:22:23 -0500

    Converted Zend_Controller to Zend\Controller, with tests Converted Zend_View to Zend\View, with tests Converted Zend_Layout to Zend\Layout, with tests

--- tests/Zend/Layout/TestAsset/MinimalCustomView.php
+++ tests/Zend/Layout/TestAsset/MinimalCustomView.php
@@ -21,13 +21,18 @@
  */
 
 /**
+ * @namespace
+ */
+namespace ZendTest\Layout\TestAsset;
+
+/**
  * @category   Zend
  * @package    Zend_Layout
  * @subpackage UnitTests
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class Zend_Layout_Test_MinimalCustomView implements Zend_View_Interface
+class MinimalCustomView implements \Zend\View\ViewInterface
 {
 
     public function getEngine() {}
