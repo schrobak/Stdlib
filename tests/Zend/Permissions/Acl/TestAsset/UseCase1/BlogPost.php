d56cc556ff3fc28bbff4f53e30d0cf73765e6c48
Matthew Weier O'Phinney <matthew@zend.com>
2012-07-24 14:09:43 -0500

    Moved Acl under new Permissions namespace
    - Per discussion on ML

--- tests/Zend/Permissions/Acl/TestAsset/UseCase1/BlogPost.php
+++ tests/Zend/Permissions/Acl/TestAsset/UseCase1/BlogPost.php
@@ -5,18 +5,18 @@
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
  * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Acl
+ * @package   Zend_Permissions
  */
 
-namespace ZendTest\Acl\TestAsset\UseCase1;
+namespace ZendTest\Permissions\Acl\TestAsset\UseCase1;
 
-use Zend\Acl\Role;
+use Zend\Permissions\Acl\Resource;
 
-class User implements Role\RoleInterface
+class BlogPost implements Resource\ResourceInterface
 {
-    public $role = 'guest';
-    public function getRoleId()
+    public $owner = null;
+    public function getResourceId()
     {
-        return $this->role;
+        return 'blogPost';
     }
 }
